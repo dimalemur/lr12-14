1711d7b535bf267424cb2e8b25443ef6f19b9b76
unknown <dimik120899@gmail.com>
2020-06-11 18:23:53 +0300

    added 12,14 labs

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1 class="jumbotron text-center">Notebook</h1>
+    <Contacts />
+  </div>
+</template>
+
+<script>
+import Сontacts from "./components/contacts.vue";
+
+export default {
+  name: "App",
+  components: {
+    Contacts: Сontacts
+  }
+};
+</script>
